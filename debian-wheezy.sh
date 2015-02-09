f10b5f6537bfb50ea5d671dde19897fb2d7fbbf0
Josua Mayer <privacy@not.given>
2015-02-09 22:31:10 +0100

    add wheezy script

--- debian-wheezy.sh
+++ debian-wheezy.sh
@@ -4,10 +4,10 @@
 source functions.inc
 
 # SETTINGS
-DEB_MIRROR=http://ports.ubuntu.com/ubuntu-ports
-DEB_RELEASE=trusty
+DEB_MIRROR=http://ftp.debian.org/debian
+DEB_RELEASE=wheezy
 DEB_ARCH=armhf
-DEB_EXTRA_PKGS="openssh-server"
+DEB_EXTRA_PKGS="openssh-server sudo"
 DUSER=solidrun
 DPASS=solidrun
 
@@ -20,15 +20,15 @@ debootstrap --no-check-gpg --arch=$DEB_ARCH --include="$DEB_EXTRA_PKGS" $DEB_REL
 
 # add BSP repo
 cat > build/etc/apt/sources.list.d/bsp.list << EOF
-deb http://repo.gbps.io/BSP:/Cubox-i/Ubuntu_Trusty_Tahr/ ./
-deb-src http://repo.gbps.io/BSP:/Cubox-i/Ubuntu_Trusty_Tahr/ ./
+deb http://repo.gbps.io/BSP:/Cubox-i/Debian_Wheezy/ ./
+deb-src http://repo.gbps.io/BSP:/Cubox-i/Debian_Wheezy/ ./
 EOF
-curl -k http://repo.gbps.io/BSP:/Cubox-i/Ubuntu_Trusty_Tahr/Release.key | chroot_run apt-key add -
+curl -k http://repo.gbps.io/BSP:/Cubox-i/Debian_Wheezy/Release.key | chroot_run apt-key add -
 
 # pin packages from bsp repo with higher priority
 cat > build/etc/apt/preferences.d/bsp-cubox-i << EOF
 Package: *
-Pin: release o=obs://mx6bs/BSP:Cubox-i/Ubuntu_Trusty_Tahr
+Pin: release o=obs://mx6bs/BSP:Cubox-i/Debian_Wheezy
 Pin-Priority: 600
 EOF
 
@@ -51,16 +51,14 @@ rm -f build/etc/resolv.conf
 
 # add update repos
 cat >> build/etc/apt/sources.list << EOF
-deb http://ports.ubuntu.com/ubuntu-ports trusty-security main
-deb http://ports.ubuntu.com/ubuntu-ports trusty-updates main
+deb http://ftp.debian.org/debian wheezy-updates main
 EOF
 
 # add source repos
 cat >> build/etc/apt/sources.list << EOF
 
-deb-src http://ports.ubuntu.com/ubuntu-ports trusty main
-deb-src http://ports.ubuntu.com/ubuntu-ports trusty-security main
-deb-src http://ports.ubuntu.com/ubuntu-ports trusty-updates main
+deb-src http://ftp.debian.org/debian wheezy main
+deb-src http://ftp.debian.org/debian wheezy-updates main
 EOF
 
 # populate fstab
@@ -79,11 +77,11 @@ EOF
 # PACKING
 # make tarball
 cd build
-tar --numeric-owner -cpvf ../ubuntu-trusty.tar *
+tar --numeric-owner -cpvf ../debian-wheezy.tar *
 cd ..
 
 # compress
-pigz ubuntu-trusty.tar
+pigz debian-wheezy.tar
 
 # CLEANUP
 
