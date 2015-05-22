1ca32900ce8d8a2f6c93ace62b5aa443201b0c30
Josua Mayer <privacy@not.given>
2015-05-22 15:30:16 +0200

    add script to create actual disk images

---
+++ mkimage.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# args
+if [ $# != 2 ]; then
+	echo "Usage: $0 <system-archive> <size_in_megabytes>"
+	exit 1
+fi
+archive=$1
+size=$2
+
+# check system
+if [ "$UID" != 0 ]; then
+	echo "root privileges required!"
+	exit 1
+fi
+
+progs="dd losetup fdisk tar mkfs.ext4"
+for prog in $progs; do
+	which $prog 1>/dev/null 2>/dev/null
+	if [ $? != 0 ]; then
+		echo "Missing $prog!"
+		exit 1
+	fi
+done
+
+# initialize temporary variables
+IMG=
+LODEV=
+MOUNT=
+
+# create exit handler
+function cleanup() {
+	if [ ! -z "$MOUNT" ]; then
+		umount ${LODEV}p1 || true
+		rmdir $MOUNT || true
+	fi
+	if [ ! -z "$LODEV" ]; then
+		losetup -d $LODEV || true
+	fi
+	if [ ! -z "$IMG" ] && [ -e "$IMG" ]; then
+		rm -f "$IMG" || true
+	fi
+#	echo cleaned up
+}
+trap cleanup INT TERM EXIT
+
+# create image file
+IMG=linux.img
+count=`echo $size/4 | bc`
+printf "Creating %s with a size of %sM: " $IMG $size
+dd if=/dev/zero of="$IMG" bs=4M count=$count 1>/dev/null 2>/dev/null
+printf "Done\n"
+
+# attach image to loopback device
+printf "Attaching image to unused loopback device: "
+LODEV=`losetup -f`
+losetup $LODEV "$IMG"
+test $? != 0 && exit 1
+printf "Done\n"
+
+# create partition
+# optimize for sdcards, and start at block 8192
+printf "Creating partition table: "
+echo "o
+n
+p
+1
+8192
+
+w
+q" | sudo fdisk $LODEV 1>/dev/null 2>/dev/null
+printf "Done\n"
+
+# reload partition table
+partprobe $LODEV
+
+# create filesystem
+printf "Creating new ext4 filesystem: "
+mkfs.ext4 -L rootfs ${LODEV}p1 1>/dev/null 2>/dev/null
+test $? != 0 && exit 1
+printf "Done\n"
+
+# mount filesystem
+MOUNT=linux
+mkdir $MOUNT
+mount ${LODEV}p1 linux
+
+# install files
+printf "Unpacking rootfs into image: "
+tar -C $MOUNT -xf $archive
+test $? != 0 && exit 1
+printf "Done\n"
+
+# install u-boot
+printf "Installing bootloader: "
+dd if=$MOUNT/boot/cubox-i-spl.bin of=$LODEV bs=1K seek=1 1>/dev/null 2>/dev/null
+dd if=$MOUNT/boot/u-boot.img of=$LODEV bs=1K seek=42 1>/dev/null 2>/dev/null
+printf "Done\n"
+
+# flush caches
+printf "Flushing kernel filesystem caches: "
+sync
+printf "Done\n"
+
+# umount
+umount ${LODEV}p1
+rmdir $MOUNT
+MOUNT=
+
+# detach loopback device
+losetup -d $LODEV
+LODEV=
+IMG=
+
+# done
+echo "Finished creating image."
+exit 0
