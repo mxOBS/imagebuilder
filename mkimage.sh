c9a816c110b06b3fe2b8079f6a54843199b4c3ad
Josua Mayer <josua.mayer97@gmail.com>
2016-07-03 18:45:15 +0200

    mkimage: be quiet

--- mkimage.sh
+++ mkimage.sh
@@ -96,8 +96,7 @@ partprobe $LODEV
 
 # create filesystem
 printf "Creating new ext4 filesystem: "
-#mkfs.ext4 -L rootfs ${LODEV}p1 1>/dev/null 2>/dev/null
-mkfs.ext4 -L rootfs ${LODEV}p1
+mkfs.ext4 -L rootfs ${LODEV}p1 1>/dev/null 2>/dev/null
 test $? != 0 && printf "Failed\n" && exit 1
 printf "Done\n"
 
